ea718ddb093349c134ade4685882ba998c408a11
Barry O Sullivan <barry@tercet.io>
2016-10-24 01:50:34 +0100

    Made it easy to add new types of ASTs for decoding.

--- ast/jsonEncoding.go
+++ ast/jsonEncoding.go
@@ -31,21 +31,7 @@ func (a *Ast) UnmarshalJSON(data []byte) error {
 		log.Fatalln("error:", err)
 	}
 
-	var dst interface{};
-	switch gAst.Kind {
-	case "ast.ExpBlock":
-		dst = new(ExpBlock)
-	case "ast.NullExp":
-		dst = new(NullExp)
-	case "ast.TrueLiteral":
-		dst = new(TrueLiteral)
-	case "ast.FalseLiteral":
-		dst = new(FalseLiteral)
-	case "ast.Return":
-		dst = new(Return)
-	case "ast.If":
-		dst = new(If)
-	}
+	var dst interface{} = gAst.exp();
 
 	if (dst == nil) {
 		fmt.Println(string(data));
@@ -55,4 +41,17 @@ func (a *Ast) UnmarshalJSON(data []byte) error {
 	json.Unmarshal(gAst.Exp, dst)
 	a.Exp = dst.(Exp);
 	return nil
+}
+
+var kindHandlers = map[string]func() interface{}{
+	"ast.ExpBlock": func() interface{} { return new(ExpBlock) },
+	"ast.NullExp": func() interface{} { return new(NullExp) },
+	"ast.TrueLiteral": func() interface{} { return new(TrueLiteral) },
+	"ast.FalseLiteral": func() interface{} { return new(FalseLiteral) },
+	"ast.Return": func() interface{} { return new(Return) },
+	"ast.If": func() interface{} { return new(If) },
+}
+
+func (a *genericAst) exp() interface{} {
+	return kindHandlers[a.Kind]();
 }
